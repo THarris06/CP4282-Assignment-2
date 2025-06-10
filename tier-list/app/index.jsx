c7ab660ab020e1b592e53f7f5693898cc66e3646
THarris06 <144939694+THarris06@users.noreply.github.com>
2025-06-10 13:52:14 -0230

    almost done

--- tier-list/app/index.jsx
+++ tier-list/app/index.jsx
@@ -4,6 +4,12 @@ import { useState } from "react";
 import Game from "../components/Game.jsx";
 import gameData from "../assets/games.json"
 
+const ds2 = require("../assets/images/dark-souls-II.png");
+const smw = require("../assets/images/super-mario-world.png");
+const mgrr = require("../assets/images/mgr-revengeance.png");
+
+const images = { "ds2": ds2, "smw": smw, "mgrr": mgrr };
+
 export default function Index() {
 
   const [index, setIndex] = useState(0);
@@ -16,8 +22,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-        <Game gameData={gameData[index]}/>
-        <Button title="Switch game" onPress={ () => setIndex( (index + 1) % gameData.length)}/>
+        <Game gameData={gameData[index]} image={images[gameData[index].image]}/>
+        <Button title="#1" onPress={ () => setIndex( (0) )}/>
+        <Button title="#2" onPress={ () => setIndex( (1) )}/>
+        <Button title="#3" onPress={ () => setIndex( (2) )}/>
     </View>
   );
 }
